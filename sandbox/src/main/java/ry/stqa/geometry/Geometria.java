219ee036eec7e4634a0227bc0562d621c8d46ff9
Peter Ignatov <peetrignatov@yandex.ru>
2024-12-19 01:30:55 +0300

    class and packega

---
+++ sandbox/src/main/java/ry/stqa/geometry/Geometria.java
@@ -0,0 +1,16 @@
+package ry.stqa.geometry;
+
+import ry.stqa.geometry.figures.Rectangle;
+import ry.stqa.geometry.figures.Square;
+
+public class Geometria {
+    public static void main(String[] args) {
+        Square.printSquareArea(7.0);
+        Square.printSquareArea(5.0);
+
+    Rectangle.printRectangleArea(3.0, 5.0);
+
+
+    }
+
+}
---
+++ sandbox/src/main/java/ry/stqa/geometry/figures/Square.java
@@ -0,0 +1,11 @@
+package ry.stqa.geometry.figures;
+
+public class Square {
+    public static void printSquareArea(double side) {
+        System.out.println("Площадь квадрата со стороной " + side + " = " + squareArea(side));
+    }
+
+    private static double squareArea(double a) {
+        return a * a;
+    }
+}
